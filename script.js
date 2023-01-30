0e773efd4080d11117c15688f1b0b6a5a891e24e
erhanatalan <erhanatalan2013@gmail.com>
2023-01-31 00:30:01 +0300

    degisiklik

--- script.js
+++ script.js
@@ -1,53 +1,56 @@
 //! Variables and selectors
 
 const textarea = document.getElementById("textarea");
-const messageDiv = document.querySelector('.input-record');
-const left_liste = document.querySelector(".left-list");
+const messageDiv = document.querySelector(".input-record");
+const left_list = document.querySelector(".left-list");
 const li = document.querySelector(".newLi");
-console.log(li);
+//console.log(li);
 const add = document.querySelector("#save");
-let now = new Date().toString().split("GMT+0300 (GMT+03:00)")[0]
-let userSelect;
-let count=localStorage.getItem("clickcount");
-console.log(count);
+const del = document.querySelector("#delete");
 
-//! Event Listeners
-add.addEventListener("click",(e)=>{
-    if(e.target.getAttribute("id")=="save"){
-        userSelect = e.target.getAttribute("id")
-        creatDiv()
-    }
-})
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+// console.log(todoList);
 
-left_liste.addEventListener("click",(ev)=>{
-    if(ev.target.tagName="li"){
-        ev.target.classList.toggle("checked")
-    }
-})
+//load event vs. DomContentLoaded
+window.addEventListener("load", () => {
+    getTodoListFromLocalStorage();
+});
+const getTodoListFromLocalStorage = () => {
+    //get TodoList from localStorage and load to UI
+    // todoList.forEach((todo)=>{
+    //     createTodo(todo);
+    // });
+}
+
+add.addEventListener("click", (e) => {
+  createDiv();
+  
+});
 
 
 //! Functions
 
-function creatDiv(){
-    const newList = document.createElement("li");
-    newList.className="newLi";
-    const dolist = document.createTextNode(textarea.value);
-    newList.appendChild(dolist);
-    left_liste.appendChild(newList);
-
-    const span= document.createElement("span");
-    const nowq = document.createTextNode(now);
-    span.className="date";
-    span.appendChild(nowq);
-    newList.appendChild(span);
-    //localStorage.setItem(`list ${count}`, JSON.stringify(newList))
-    //window.location.reload()
-}
-
-function clickCounter() {
-  if (localStorage.clickcount) {
-    localStorage.clickcount = Number(localStorage.clickcount)+1;
-  } else {
-    localStorage.clickcount = 1;
+const createDiv = () =>{
+  const newTodo = {
+    id: new Date().getTime(),
+    text: textarea.value,
+    todo: false,
+    arshive:false
   }
+  todoList.push(newTodo)
+  console.log(todoList);
+  localStorage.setItem("todoList", JSON.stringify(todoList))
+
+  left_list.innerHTML += `
+  <li id="${newTodo.id}" class="text-left fa-1x">
+    <i class="fa-solid fa-check text-danger"></i>
+    <span class="textLi text-dark ">${textarea.value}</span>
+    <i class="fa-solid fa-trash-can text-danger"></i>
+  </li>
+  `
 }
+
+
+
+
+
